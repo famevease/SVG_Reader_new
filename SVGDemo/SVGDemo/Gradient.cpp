856a7868b73d5e9bd6363d1fa08959537a239984
nguyen-1610 <duynguyenhuynh250@gmail.com>
2025-07-10 15:19:10 +0700

    add Graphics

--- SVGDemo/SVGDemo/Gradient.cpp
+++ SVGDemo/SVGDemo/Gradient.cpp
@@ -1,2 +1,25 @@
 #include "stdafx.h"
+
 #include "Gradient.h"
+Gradient::Gradient(const std::vector< Stop >& stops,
+                   const std::pair< Vector2Df, Vector2Df >& points,
+                   const std::string& units)
+    : stops(stops), points(points), units(units) {}
+
+std::vector< Stop > Gradient::getStops() const { return stops; }
+
+std::pair< Vector2Df, Vector2Df > Gradient::getPoints() const { return points; }
+
+void Gradient::setUnits(const std::string& units) { this->units = units; }
+
+std::string Gradient::getUnits() const { return units; }
+
+void Gradient::setTransforms(const std::vector< std::string >& transforms) {
+    this->transforms = transforms;
+}
+
+std::vector< std::string > Gradient::getTransforms() const {
+    return transforms;
+}
+
+void Gradient::addStop(Stop stop) { stops.push_back(stop); }
