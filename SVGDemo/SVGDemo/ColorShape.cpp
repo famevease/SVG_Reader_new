1f1aede5968397b83886fa6a7d1afb57f0a2e8c3
Hireophant <tntdanh2506@gmail.com>
2025-07-11 22:44:04 +0700

    color

--- SVGDemo/SVGDemo/ColorShape.cpp
+++ SVGDemo/SVGDemo/ColorShape.cpp
@@ -1,28 +1,37 @@
 #include "stdafx.h"
 #include "ColorShape.h"
-using namespace std;
 
-stroke::stroke() {
-	this->strokeWidth = 1;
+using namespace Gdiplus;
+
+const ColorShape ColorShape::Black(Color(255, 0, 0, 0));
+const ColorShape ColorShape::White(Color(255, 255, 255, 255));
+const ColorShape ColorShape::Red(Color(255, 255, 0, 0));
+const ColorShape ColorShape::Green(Color(255, 0, 255, 0));
+const ColorShape ColorShape::Blue(Color(255, 0, 0, 255));
+const ColorShape ColorShape::Yellow(Color(255, 255, 255, 0));
+const ColorShape ColorShape::Magenta(Color(255, 255, 0, 255));
+const ColorShape ColorShape::Cyan(Color(255, 0, 255, 255));
+const ColorShape ColorShape::Transparent(Color(0, 0, 0, 0));
+
+ColorShape::ColorShape() : color(255, 0, 0, 0) {}
+
+ColorShape::ColorShape(BYTE red, BYTE green, BYTE blue, BYTE alpha)
+	: color(alpha, red, green, blue) {}
+
+ColorShape::ColorShape(const Color &c) : color(c) {}
+
+BYTE ColorShape::getR() const { return color.GetRed(); }
+BYTE ColorShape::getG() const { return color.GetGreen(); }
+BYTE ColorShape::getB() const { return color.GetBlue(); }
+BYTE ColorShape::getA() const { return color.GetAlpha(); }
+
+Color ColorShape::toGDIColor() const { return color; }
+
+std::ostream &operator<<(std::ostream &os, const ColorShape &color)
+{
+	os << "Color(" << static_cast<int>(color.getR()) << ", "
+	   << static_cast<int>(color.getG()) << ", "
+	   << static_cast<int>(color.getB()) << ", "
+	   << static_cast<int>(color.getA()) << ")";
+	return os;
 }
-
-stroke::~stroke() {
-	this->strokeWidth = 0;
-}
-
-ColorShape stroke::getStrokeColor() {
-	return this->strokeColor;
-}
-
-float stroke::getStrokeWidth() {
-	return this->strokeWidth;
-}
-
-void stroke::setStrokeColor(ColorShape strokeColor) {
-	this->strokeColor = strokeColor;
-}
-
-void stroke::setStrokeWidth(float strokeWidth) {
-	this->strokeWidth = strokeWidth;
-}
-
