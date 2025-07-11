4754c28b858699ca50841dd0ddfec8c19c78a20b
famevease <trangiahien2006@gmail.com>
2025-07-11 13:41:36 +0700

    ViewBox

---
+++ SVGDemo/SVGDemo/Viewbox.cpp
@@ -0,0 +1,14 @@
+#include "stdafx.h"
+#include "Viewbox.h"
+
+ViewBox::ViewBox() : x(0), y(0), w(0), h(0) {}
+
+ViewBox::ViewBox(float X, float Y, float W, float H) : x(X), y(Y), w(W), h(H) {}
+
+float ViewBox::getX() const { return x; }
+
+float ViewBox::getY() const { return y; }
+
+float ViewBox::getWidth() const { return w; }
+
+float ViewBox::getHeight() const { return h; }
