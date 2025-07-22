7eadfcbdb70617155e5e8a6d9a904f95735c31aa
thandan1241 <thanphan2022@gmail.com>
2025-07-22 17:15:52 +0700

    Chỉnh lại tên hàm cho gống (MyEllipse)

--- SVGDemo/SVGMain/Circle.cpp
+++ SVGDemo/SVGMain/Circle.cpp
@@ -1,7 +1,7 @@
 #include "stdafx.h"
 #include "Circle.h"
 
-Circle::Circle(float radius, const Vector2Df& center, ColorShape fill, ColorShape stroke, float stroke_width): Ellipse(Vector2Df(radius, radius), center, fill, stroke, stroke_width) {}
+Circle::Circle(float radius, const Vector2Df& center, ColorShape fill, ColorShape stroke, float stroke_width): MyEllipse(Vector2Df(radius, radius), center, fill, stroke, stroke_width) {}
 
 std::string Circle::getClass() const { 
 	return "Circle"; 
