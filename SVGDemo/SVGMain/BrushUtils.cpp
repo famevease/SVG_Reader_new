f15dd52c93361d5d238be5dfba1e57cd04133612
cpm-201 <chau.pmy@gmail.com>
2025-07-24 13:03:30 +0700

    tách BrushUtils từ Renderer, fix bug sau

---
+++ SVGDemo/SVGMain/BrushUtils.cpp
@@ -0,0 +1,184 @@
+#include "BrushUtils.h"
+
+// Apply transformation matrix operations on a linear gradient brush
+void BrushUtils::applyTransformsOnBrush(
+    std::vector<std::string> transform_order,
+    Gdiplus::LinearGradientBrush*& brush) const {
+    for (auto type : transform_order) {
+        if (type.find("translate") != std::string::npos) {
+            // Apply translation transformation
+            float trans_x = getTranslate(type).first,
+                  trans_y = getTranslate(type).second;
+            brush->TranslateTransform(trans_x, trans_y);
+        } else if (type.find("rotate") != std::string::npos) {
+            // Apply rotation transformation
+            float degree = getRotate(type);
+            brush->RotateTransform(degree);
+        } else if (type.find("scale") != std::string::npos) {
+            // Apply scaling transformation
+            if (type.find(",") != std::string::npos) {
+                float scale_x = getScaleXY(type).first,
+                      scale_y = getScaleXY(type).second;
+                brush->ScaleTransform(scale_x, scale_y);
+            } else {
+                float scale = getScale(type);
+                brush->ScaleTransform(scale, scale);
+            }
+        } else if (type.find("matrix") != std::string::npos) {
+            // Apply matrix transformation
+            float a = 0, b = 0, c = 0, d = 0, e = 0, f = 0;
+            if (type.find(",") != std::string::npos) {
+                type.erase(std::remove(type.begin(), type.end(), ','),
+                           type.end());
+            }
+            sscanf_s(type.c_str(), "matrix(%f %f %f %f %f %f)", &a, &b, &c, &d,
+                   &e, &f);
+            Gdiplus::Matrix matrix(a, b, c, d, e, f);
+            brush->SetTransform(&matrix);
+        }
+    }
+}
+
+// Apply transformation matrix operations on a path gradient brush
+void BrushUtils::applyTransformsOnBrush(
+    std::vector< std::string > transform_order,
+    Gdiplus::PathGradientBrush*& brush) const {
+    for (auto type : transform_order) {
+        if (type.find("translate") != std::string::npos) {
+            float trans_x = getTranslate(type).first,
+                  trans_y = getTranslate(type).second;
+            brush->TranslateTransform(trans_x, trans_y);
+        } else if (type.find("rotate") != std::string::npos) {
+            float degree = getRotate(type);
+            brush->RotateTransform(degree);
+        } else if (type.find("scale") != std::string::npos) {
+            if (type.find(",") != std::string::npos) {
+                float scale_x = getScaleXY(type).first,
+                      scale_y = getScaleXY(type).second;
+                brush->ScaleTransform(scale_x, scale_y);
+            } else {
+                float scale = getScale(type);
+                brush->ScaleTransform(scale, scale);
+            }
+        } else if (type.find("matrix") != std::string::npos) {
+            float a = 0, b = 0, c = 0, d = 0, e = 0, f = 0;
+            if (type.find(",") != std::string::npos) {
+                type.erase(std::remove(type.begin(), type.end(), ','),
+                           type.end());
+            }
+            sscanf_s(type.c_str(), "matrix(%f %f %f %f %f %f)", &a, &b, &c, &d,
+                   &e, &f);
+            Gdiplus::Matrix matrix(a, b, c, d, e, f);
+            brush->SetTransform(&matrix);
+        }
+    }
+}
+
+// Get the Gdiplus::Brush for rendering an SVG element (shape) with a gradient
+// or solid color
+Gdiplus::Brush* BrushUtils::getBrush(SVGElement* shape,
+                                   Gdiplus::RectF bound) const {
+    Gradient* gradient = shape->getGradient();
+    if (gradient != NULL) {
+        std::pair< Vector2Df, Vector2Df > points = gradient->getPoints();
+        std::vector< Stop > stops = gradient->getStops();
+        int stop_size = stops.size() + 2;
+        Gdiplus::Color* colors = new Gdiplus::Color[stop_size];
+        float* offsets = new float[stop_size];
+
+        if (gradient->getClass() == "LinearGradient") {
+            // Brush linear gradient
+            if (gradient->getUnits() == "objectBoundingBox") {
+                points.first.x = bound.X;
+                points.first.y = bound.Y;
+                points.second.x = bound.X + bound.Width;
+                points.second.y = bound.Y + bound.Height;
+            }
+
+            // Set the center color
+            offsets[0] = 0;
+            offsets[stop_size - 1] = 1;
+            colors[0] =
+                Gdiplus::Color(stops[0].getColor().getA(), stops[0].getColor().getR(),
+                               stops[0].getColor().getG(), stops[0].getColor().getB());
+            colors[stop_size - 1] =
+                Gdiplus::Color(stops[stop_size - 3].getColor().getA(),
+                               stops[stop_size - 3].getColor().getR(),
+                               stops[stop_size - 3].getColor().getG(),
+                               stops[stop_size - 3].getColor().getB());
+
+            // Reverse the order of the stops
+            for (size_t i = 1; i < stop_size - 1; ++i) {
+                colors[i] = Gdiplus::Color(
+                    stops[i - 1].getColor().getA(), stops[i - 1].getColor().getR(),
+                    stops[i - 1].getColor().getG(), stops[i - 1].getColor().getB());
+                offsets[i] = stops[i - 1].getOffset();
+            }
+
+            // Create the brush of linear gradient
+            Gdiplus::LinearGradientBrush* fill =
+                new Gdiplus::LinearGradientBrush(
+                    Gdiplus::PointF(points.first.x, points.first.y),
+                    Gdiplus::PointF(points.second.x, points.second.y),
+                    colors[0], colors[stop_size - 1]);
+            fill->SetWrapMode(Gdiplus::WrapModeTileFlipX);
+            fill->SetInterpolationColors(colors, offsets, stop_size);
+            applyTransformsOnBrush(gradient->getTransforms(), fill);
+
+            delete[] colors;
+            delete[] offsets;
+            return fill;
+        } else if (gradient->getClass() == "RadialGradient") {
+            // Brush radiol gradient
+            RadialGradient* radial_gradient =
+                dynamic_cast< RadialGradient* >(gradient);
+            Vector2Df radius = radial_gradient->getRadius();
+
+            // If the gradient is in userSpaceOnUse, the radius is the distance
+            if (gradient->getUnits() == "userSpaceOnUse") {
+                bound.X = points.first.x - radius.x;
+                bound.Y = points.first.y - radius.x;
+                bound.Width = radius.x * 2;
+                bound.Height = radius.x * 2;
+            }
+
+            Gdiplus::GraphicsPath path;
+            path.AddEllipse(bound);
+            Gdiplus::PathGradientBrush* fill =
+                new Gdiplus::PathGradientBrush(&path);
+
+            // Set the center color
+            offsets[0] = 0;
+            offsets[stop_size - 1] = 1;
+            colors[0] = Gdiplus::Color(stops[stop_size - 3].getColor().getA(),
+                                       stops[stop_size - 3].getColor().getR(),
+                                       stops[stop_size - 3].getColor().getG(),
+                                       stops[stop_size - 3].getColor().getB());
+            colors[stop_size - 1] =
+                Gdiplus::Color(stops[0].getColor().getA(), stops[0].getColor().getR(),
+                               stops[0].getColor().getG(), stops[0].getColor().getB());
+
+            // Reverse the order of the stops
+            for (size_t i = 1; i < stop_size - 1; ++i) {
+                colors[i] =
+                    Gdiplus::Color(stops[stop_size - 2 - i].getColor().getA(),
+                                   stops[stop_size - 2 - i].getColor().getR(),
+                                   stops[stop_size - 2 - i].getColor().getG(),
+                                   stops[stop_size - 2 - i].getColor().getB());
+                offsets[i] = 1 - stops[stop_size - 2 - i].getOffset();
+            }
+
+            fill->SetInterpolationColors(colors, offsets, stop_size);
+            applyTransformsOnBrush(gradient->getTransforms(), fill);
+            delete[] colors;
+            delete[] offsets;
+            return fill;
+        }
+    } else {
+        ColorShape color = shape->getFillColor();
+        Gdiplus::SolidBrush* fill = new Gdiplus::SolidBrush(
+            Gdiplus::Color(color.getA(), color.getR(), color.getG(), color.getB()));
+        return fill;
+    }
+    return nullptr;
+}
---
+++ SVGDemo/SVGMain/BrushUtils.h
@@ -0,0 +1,48 @@
+#ifndef BRUSHUTILS_H
+#define BRUSHUTILS_H
+
+#include <gdiplus.h>
+#include <vector>
+#include <string>
+#include "SVGElement.h"
+#include "TransformUtils.h"
+//#include "LinearGradient.h"
+//#include "RadialGradient.h"
+
+class BrushUtils {
+private:
+    /**
+     * @brief Utility function to apply a series of transformations to the brush
+     * object.
+     *
+     * @param transform_order The order in which transformations should be
+     * applied.
+     * @param brush The Gdiplus::LinearGradientBrush object for the shape fill.
+     */
+    void applyTransformsOnBrush(std::vector<std::string> transform_order, 
+                                Gdiplus::LinearGradientBrush*& brush) const;
+
+    /**
+     * @brief Utility function to apply a series of transformations to the brush
+     * object.
+     *
+     * @param transform_order The order in which transformations should be
+     * applied.
+     * @param brush The Gdiplus::PathGradientBrush object for the shape fill.
+     *
+     */
+    void applyTransformsOnBrush(std::vector<std::string>& transform_order,
+                                 Gdiplus::PathGradientBrush*& brush) const;
+
+    /**
+     * @brief Gets the Gdiplus::brush object for the shape fill.
+     *
+     * @param shape The SVGElement representing the shape.
+     * @param bound The bounding box of the shape.
+     * @return The Gdiplus::brush object for the shape fill.
+     */
+    Gdiplus::Brush* getBrush(SVGElement* shape, Gdiplus::RectF bound) const;
+
+};
+
+#endif
